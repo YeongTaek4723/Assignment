befa329a08cbce9d07d882cf3d59e3ac9a0047ba
YeongTaek <neuro0108@gmil.com>
2023-08-03 14:59:20 +0900

    Service

--- GamesSpot/index.js
+++ GamesSpot/index.js
@@ -1,4 +1,8 @@
+#!/usr/bin/node
 const express = require('express');
+const mysql = require('mysql');
+const dbconfig = require('./config/dbinfo.js');
+const connection = mysql.createConnection(dbconfig);
 
 const app = express();
 const port = 3000;
@@ -6,21 +10,27 @@ const path = require('path');
 const cors = require('cors');
 
 app.use(
-        cors({
-                origin:'http://13.125.71.97:3000',
-                credentials : true,
-        })
+	cors({
+		origin:'http://13.125.71.97:3000',
+		credentials : true,
+	})
 );
 
 app.use(express.static('front'));
 
+app.get('/api', (req, res) => {
+	connection.query('SELECT * FROM GamesSpot', (error, rows) => {
+		if (error) throw error;
+		res.send(rows);
+	});
+});
+
 app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, './', 'front', 'main.html'));
+	res.sendFile(path.join(__dirname, './', 'front', 'main.html'));
 });
 
 
 
 app.listen(port, () => {
-        console.log(`Example app listening on port ${port}`);
+	console.log(`Example app listening on port ${port}`);
 });
-
