e8df4d762c08df8de154374e84142808889a1111
Ubuntu <ubuntu@ip-172-31-10-207.ap-northeast-2.compute.internal>
2023-08-08 11:27:14 +0000

    server

--- GamesSpot/index.js
+++ GamesSpot/index.js
@@ -9,6 +9,24 @@ const port = 3000;
 const path = require('path');
 const cors = require('cors');
 
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
+app.set('view engine', 'ejs');
+app.set('view', path.join(__dirname));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(
+	session({
+		secret: 'g7$In!2@S#%90c$5mB',
+		resave: true,
+		saveUninitialized: true
+	})
+);
+
 app.use(
 	cors({
 		origin:'http://13.125.71.97:3000',
@@ -26,9 +44,17 @@ app.get('/api', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, './', 'front', 'main.html'));
+	res.sendFile(path.join(__dirname + './front/main.html'));
 });
 
+app.get('/login', (req, res) => {
+	res.sendFile(path.join(__dirname + './front/login.html'));
+});
+
+
+app.get('/collection', (req, res) => {
+        res.sendFile(path.join(__dirname + './front/collection_info.html'));
+});
 
 
 app.listen(port, () => {
